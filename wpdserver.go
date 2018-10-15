f5a9ebc8fb3b47d62e7024d0cf36b45ca71dba80
petermeissner <retep.meissner@gmail.com>
2018-10-15 15:17:21 +0200

    put credentials in separate file

--- wpdserver.go
+++ wpdserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -10,12 +11,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	DB_USER     = "****"
-	DB_PASSWORD = "****"
-	DB_NAME     = "****"
-	DB_HOST     = "****"
-)
+// read in database credentials
+var db_credentials, err = ioutil.ReadFile(".db_credentials")
 
 func main() {
 
@@ -42,21 +39,36 @@ func api_index(http_out http.ResponseWriter, r *http.Request) {
 func api_article(http_out http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(http_out, "{article}")
 
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s",
-		DB_USER, DB_PASSWORD, DB_NAME, DB_HOST)
-	db, err := sql.Open("postgres", dbinfo)
+	vars := mux.Vars(r)
+	article := vars["article"]
+
+	db, err := sql.Open("postgres", string(db_credentials))
 	checkErr(err)
 	defer db.Close()
 
-	rows, err := db.Query("SELECT distinct page_view_date  FROM import_jobs;")
+	rows, err := db.Query(
+		`select 
+			a.page_id, a.page_name, b.year, b.page_views 
+			from 
+			    (select * from dict_en where page_name = $1) as a 
+                            left join imports_en as b on a.page_id = b.page_id 
+			;`, article)
 	checkErr(err)
-	fmt.Fprintln(http_out, "page_view_date")
 
 	for rows.Next() {
-		var page_view_date string
-		err = rows.Scan(&page_view_date)
+		var page_id string
+		var page_name string
+		var year string
+		var page_views string
+		err = rows.Scan(&page_id, &page_name, &year, &page_views)
 		checkErr(err)
-		fmt.Fprintf(http_out, "%s\n", page_view_date)
+		fmt.Fprintf(http_out, "{")
+		fmt.Fprintf(http_out, "\n  \"search\": \"%s\",", article)
+		fmt.Fprintf(http_out, "\n  \"page_id\": %s,", page_id)
+		fmt.Fprintf(http_out, "\n  \"page_name\": \"%s\",", page_name)
+		fmt.Fprintf(http_out, "\n  \"year\": %s,", year)
+		fmt.Fprintf(http_out, "\n  \"page_views\": [%s]", page_views)
+		fmt.Fprintf(http_out, "\n}")
 	}
 }
 
