d295f1593d5b677779f67b192c21520b718786be
petermeissner <retep.meissner@gmail.com>
2018-10-15 15:19:40 +0200

    json putput

--- wpdserver.go
+++ wpdserver.go
@@ -37,43 +37,67 @@ func api_index(http_out http.ResponseWriter, r *http.Request) {
 }
 
 func api_article(http_out http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(http_out, "{article}")
 
+	// inform http_out that content is JSON
+	http_out.Header().Add("Content-Type", "application/json")
+
+	// get request varaibles
 	vars := mux.Vars(r)
 	article := vars["article"]
+
+	// establish connection to databse
+	db, err := sql.Open("postgres", string(db_credentials))
+	checkErr(err)
+	defer db.Close()
+
+	// execute query
+	rows, err := db.Query(
+		`select array_to_json(array_agg(row_to_json(t))) as res_string from
+			(select 
+			a.page_id, a.page_name, b.year, b.page_views 
+			from 
+			(select * from dict_en where page_name = $1) as a 
+                            left join imports_en as b on a.page_id = b.page_id 
+			) as t 
+		;`,
+		article)
+
+	checkErr(err)
+	for rows.Next() {
+		var res_string string
+		err = rows.Scan(&res_string)
+		checkErr(err)
+		fmt.Fprintf(http_out, "%s", res_string)
+	}
+
+}
+
+func api_search(http_out http.ResponseWriter, r *http.Request) {
+	http_out.Header().Add("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	search := vars["search"]
 
 	db, err := sql.Open("postgres", string(db_credentials))
 	checkErr(err)
 	defer db.Close()
 
 	rows, err := db.Query(
-		`select 
-			a.page_id, a.page_name, b.year, b.page_views 
+		`select array_to_json(array_agg(row_to_json(t))) as res_string from
+			(select a.page_id, a.page_name, b.year, b.page_views 
 			from 
-			    (select * from dict_en where page_name = $1) as a 
+			    (select * from dict_en where page_name ~ $1) as a 
                             left join imports_en as b on a.page_id = b.page_id 
-			;`, article)
+			limit 100) as t
+			;`, search)
 	checkErr(err)
 
 	for rows.Next() {
-		var page_id string
-		var page_name string
-		var year string
-		var page_views string
-		err = rows.Scan(&page_id, &page_name, &year, &page_views)
+		var res_string string
+		err = rows.Scan(&res_string)
 		checkErr(err)
-		fmt.Fprintf(http_out, "{")
-		fmt.Fprintf(http_out, "\n  \"search\": \"%s\",", article)
-		fmt.Fprintf(http_out, "\n  \"page_id\": %s,", page_id)
-		fmt.Fprintf(http_out, "\n  \"page_name\": \"%s\",", page_name)
-		fmt.Fprintf(http_out, "\n  \"year\": %s,", year)
-		fmt.Fprintf(http_out, "\n  \"page_views\": [%s]", page_views)
-		fmt.Fprintf(http_out, "\n}")
+		fmt.Fprintf(http_out, "%s", res_string)
 	}
-}
 
-func api_search(http_out http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(http_out, "{search}")
 }
 
 func checkErr(err error) {
